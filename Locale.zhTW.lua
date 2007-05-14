e85e8e63ff0a483a5018c00d1db2de5a17b2cf3a
norova <unknown@unknown.com>
2007-05-14 04:27:54 +0000

    SilverDragon: add zhTW

---
+++ Locale.zhTW.lua
@@ -0,0 +1,38 @@
+﻿local L = AceLibrary("AceLocale-2.2"):new("SilverDragon")
+
+L:RegisterTranslations("zhTW", function() return {
+	["DefaultIcon"] = "Interface\\Icons\\INV_Misc_Head_Dragon_01",
+	["ChatCommands"] = {"/silverdragon", "/sd"},
+	
+	["Rare mob"] = "稀有怪",
+	
+	["Do scan"] = "執行掃描",
+	["Settings"] = "設定",
+	["Configuration options"] = "選項",
+	["Scan"] = "掃描",
+	["Scan for nearby rares at a regular interval"] = "掃描附近的稀有怪",
+	["Announce"] = "通知",
+	["Display a message when a rare is detected nearby"] = "發現稀有怪時顯示訊息",
+	["Chat"] = "聊天",
+	["In the chatframe"] = "於聊天視窗",
+	["Error"] = "錯誤",
+	["In the errorframe"] = "於錯誤視窗",
+	["Notes"] = "標記",
+	["Make notes in Cartographer"] = "於插件Cartographer標記",
+	["Scan for nearby rares"] = "掃描附近的稀有怪",
+	["Import defaults"] = "匯入預設",
+	["Import a default database of rares"] = "匯入預設稀有怪資料庫",
+	
+	["%s seen!"] = "看到 %s 了！",
+	["(it's dead)"] = "(死亡)",
+	
+	["Rares"] = "稀有",
+	["Never"] = "從不",
+	[" day(s)"] = " 天",
+	[" hour(s)"] = " 小時",
+	[" minute(s)"] = " 分",
+	
+	["Raretracker needs to be loaded for this to work."] = "稀有怪追蹤須被載入",
+} end)
+
+-- Traditional Chinese: 竹笙姬@暴風祭壇 http://tw.myblog.yahoo.com/norova.tw
