83608b97763329ad118a9e250186560a2b434121
ananhaid <unknown@unknown.com>
2008-03-08 10:09:15 +0000

    SilverDragon: - zhCN/zhTW DB add.

---
+++ Locale.zhCN.lua
@@ -0,0 +1,36 @@
+﻿local L = AceLibrary("AceLocale-2.2"):new("SilverDragon")
+
+L:RegisterTranslations("zhCN", function() return {
+	["DefaultIcon"] = "Interface\\Icons\\INV_Misc_Head_Dragon_01",
+	["ChatCommands"] = {"/silverdragon", "/sd"},
+	
+	["Rare mob"] = "稀有怪物",
+	
+	["Do scan"] = "进行扫描",
+	["Settings"] = "设置",
+	["Configuration options"] = "选项",
+	["Scan"] = "扫描",
+	["Scan for nearby rares at a regular interval"] = "扫描附近的稀有怪物",
+	["Announce"] = "通知",
+	["Display a message when a rare is detected nearby"] = "发现稀有怪物时显示信息",
+	["Chat"] = "聊天",
+	["In the chatframe"] = "于聊天窗口",
+	["Error"] = "错误",
+	["In the errorframe"] = "于错误窗口",
+	["Notes"] = "标记",
+	["Make notes in Cartographer"] = "于插件 Cartographer 标记",
+	["Scan for nearby rares"] = "扫描附近的稀有怪物",
+	["Import defaults"] = "导入预设",
+	["Import a default database of rares"] = "导入预设稀有怪物资料库",
+	
+	["%s seen!"] = "看到 %s 了！",
+	["(it's dead)"] = "(死亡)",
+	
+	["Rares"] = "稀有",
+	["Never"] = "从不",
+	[" day(s)"] = " 天",
+	[" hour(s)"] = " 小时",
+	[" minute(s)"] = " 分",
+	
+	["Raretracker needs to be loaded for this to work."] = "稀有怪物追踪需被载入。",
+} end)
