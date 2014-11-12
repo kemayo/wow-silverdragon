2f9c4fe5cc1450a3e6eddad86332f0207e65337c
David Lynch <kemayo@gmail.com>
2014-11-12 11:07:10 -0600

    Fix unit tests

--- tools/test.py
+++ tools/test.py
@@ -7,28 +7,31 @@ class TestNPC:
         pass
 
     def test_basics(self):
-        self.assertEqual(str(self.npc), "Zandalari Warbringer")
-        self.assertEqual(self.npc.data['level'], 92)
-        self.assertTrue(self.npc.data['elite'])
-        self.assertEqual(self.npc.data['creature_type'], "Humanoid")
-        self.assertFalse(self.npc.data['tameable'])
+        npc = self.getNPC(69842)
+        self.assertEqual(str(npc), "Zandalari Warbringer")
+        self.assertTrue(type(npc.data['level']) is int)
+        self.assertTrue(npc.data['elite'])
+        self.assertEqual(npc.data['creature_type'], "Humanoid")
+        self.assertFalse(npc.data['tameable'])
 
     def test_locations(self):
-        self.assertEqual(type(self.npc.data['locations']), dict)
-        for zoneid in self.npc.data['locations'].keys():
+        npc = self.getNPC(69842)
+        self.assertEqual(type(npc.data['locations']), dict)
+        for zoneid in npc.data['locations'].keys():
             self.assertEqual(type(zoneid), int)
-            for coord in self.npc.data['locations'][zoneid]:
+            for coord in npc.data['locations'][zoneid]:
                 self.assertEqual(type(coord), int)
                 self.assertTrue(coord > 0)
                 self.assertTrue(coord < 100000000)
 
+
 class TestWowhead(TestNPC, unittest.TestCase):
-    def setUp(self):
-        self.npc = npc.wowhead.WowheadNPC(69842)
+    def getNPC(self, id):
+        return npc.wowhead.WowheadNPC(id)
 
 class TestWowdb(TestNPC, unittest.TestCase):
-    def setUp(self):
-        self.npc = npc.wowdb.WowdbNPC(69842)
+    def getNPC(self, id):
+        return npc.wowdb.WowdbNPC(id)
 
 if __name__ == '__main__':
     unittest.main()
