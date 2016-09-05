4c487e0d7062800de20d2c0075e387e7f3188188
David Lynch <kemayo@gmail.com>
2016-09-04 21:41:12 -0500

    Handynotes tooltip colored quest-completion wrong

--- integration/handynotes.lua
+++ integration/handynotes.lua
@@ -116,9 +116,10 @@ function handler:OnEnter(mapFile, coord)
 		end
 	end
 	if core.db.global.mob_quests[id] then
+		local completed = IsQuestFlaggedCompleted(core.db.global.mob_quests[id])
 		tooltip:AddDoubleLine(
 			QUESTS_COLON,
-			IsQuestFlaggedCompleted(core.db.global.mob_quests[id]) and COMPLETE or INCOMPLETE,
+			completed and COMPLETE or INCOMPLETE,
 			1, 1, 0,
 			completed and 0 or 1, completed and 1 or 0, 0
 		)
