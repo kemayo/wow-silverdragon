fa346c90004f6975c413d8a12be5b1720800e120
David Lynch <kemayo@gmail.com>
2016-11-11 15:47:18 -0600

    Tooltips on the popup

--- outputs/clicktarget.lua
+++ outputs/clicktarget.lua
@@ -4,6 +4,16 @@ local core = LibStub("AceAddon-3.0"):GetAddon("SilverDragon")
 local module = core:NewModule("ClickTarget", "AceEvent-3.0")
 local Debug = core.Debug
 
+local CreateAnimationAlpha
+local escapes = {
+	-- |TTexturePath:size1:size2:xoffset:yoffset:dimx:dimy:coordx1:coordx2:coordy1:coordy2|t
+	leftClick = [[|TInterface\TUTORIALFRAME\UI-TUTORIAL-FRAME:19:11:-1:0:512:512:9:67:227:306|t]],
+	rightClick = [[|TInterface\TUTORIALFRAME\UI-TUTORIAL-FRAME:20:12:0:-1:512:512:9:66:332:411|t]],
+	keyDown = [[|TInterface\TUTORIALFRAME\UI-TUTORIAL-FRAME:0:0:0:-1:512:512:9:66:437:490|t]],
+	green = _G.GREEN_FONT_COLOR_CODE,
+	red = _G.RED_FONT_COLOR_CODE,
+}
+
 function module:OnInitialize()
 	self.db = core.db:RegisterNamespace("ClickTarget", {
 		profile = {
@@ -105,7 +115,7 @@ function module:ShowFrame()
 
 	local achievement, achievement_name, completed = ns:AchievementMobStatus(current.id)
 	if achievement then
-		popup.status:SetFormattedText("%s%s|r", completed and GREEN_FONT_COLOR_CODE or RED_FONT_COLOR_CODE, achievement_name)
+		popup.status:SetFormattedText("%s%s|r", completed and escapes.green or escapes.red, achievement_name)
 	else
 		popup.status:SetText("")
 	end
@@ -195,6 +205,9 @@ end
 function module.Looks:Legendary(popup)
 	popup:SetSize(302, 119)
 
+	-- left, right, top, bottom
+	popup:SetHitRectInsets(20, 0, 15, 15)
+
 	popup.background:SetSize(276, 96)
 	popup.background:SetAtlas("LegendaryToast-background", true)
 	popup.background:SetPoint("CENTER")
@@ -277,8 +290,6 @@ function module:SizeModel(popup, offset, borders)
 	model:SetPoint("TOPLEFT", popup.modelbg, offset, -offset)
 	model:SetPoint("BOTTOMRIGHT", popup.modelbg, -offset, offset)
 end
-
-local CreateAnimationAlpha
 
 function module:CreatePopup()
 	-- Set up the frame
@@ -370,6 +381,12 @@ function module:CreatePopup()
 	CreateAnimationAlpha(glow.animIn, 0, 1, 0.2, nil, 1)
 	CreateAnimationAlpha(glow.animIn, 1, 0, 0.5, nil, 2)
 
+	glow.animEnter = glow:CreateAnimationGroup()
+	glow.animEnter:SetLooping("BOUNCE")
+	glow.animEnter:SetScript("OnFinished", function(self) self:GetParent():Hide() end)
+	CreateAnimationAlpha(glow.animEnter, 0, 0.3, 0.8, nil, 1)
+	CreateAnimationAlpha(glow.animEnter, 0.3, 0, 0.8, nil, 2)
+
 	shine.animIn = shine:CreateAnimationGroup()
 	shine.animIn:SetScript("OnFinished", function(self) self:GetParent():Hide() end)
 
@@ -381,7 +398,7 @@ function module:CreatePopup()
 	shineTranslate:SetOrder(2)
 
 	-- handlers
-	popup:SetScript("OnShow", function(self)
+	popup:HookScript("OnShow", function(self)
 		self.glow:Show()
 		self.glow.animIn:Play()
 		self.shine:Show()
@@ -389,12 +406,34 @@ function module:CreatePopup()
 
 		self.animIn:Play()
 	end)
-	popup:SetScript("OnHide", function(self)
+	popup:HookScript("OnHide", function(self)
 		self.glow.animIn:Stop()
 		self.shine.animIn:Stop()
 		self.animIn:Stop()
 
 		self.model:ClearModel()
+	end)
+	popup:SetScript("OnEnter", function(self)
+		local anchor = (self:GetCenter() < (UIParent:GetWidth() / 2)) and "ANCHOR_RIGHT" or "ANCHOR_LEFT"
+		GameTooltip:SetOwner(self, anchor, 0, -60)
+		GameTooltip:AddLine(escapes.leftClick .. " " .. TARGET)
+		if module.db.profile.locked then
+			GameTooltip:AddLine(escapes.keyDown .. "ALT + " .. escapes.leftClick .. " + " .. DRAG_MODEL .. "  " .. MOVE_FRAME)
+		else
+			GameTooltip:AddLine(escapes.leftClick .. " + " .. DRAG_MODEL .. "  " .. MOVE_FRAME)
+		end
+		GameTooltip:Show()
+
+		self.glow.animIn:Stop() -- in case
+		self.glow.animEnter:Stop() -- in case
+
+		self.glow:Show()
+		self.glow.animEnter:Play()
+	end)
+	popup:SetScript("OnLeave", function(self)
+		GameTooltip:Hide()
+
+		self.glow.animEnter:Finish()
 	end)
 	popup:SetScript("OnDragStart", function(self)
 		if module:ShouldBeDraggable() then
@@ -403,6 +442,15 @@ function module:CreatePopup()
 	end)
 	popup:SetScript("OnDragStop", function(self)
 		self:StopMovingOrSizing()
+	end)
+
+	popup.close:SetScript("OnEnter", function(self)
+		GameTooltip:SetOwner(self, "ANCHOR_CURSOR", 0, 0)
+		GameTooltip:AddLine(escapes.leftClick .. " " .. CLOSE)
+		GameTooltip:Show()
+	end)
+	popup.close:SetScript("OnLeave", function(self)
+		GameTooltip:Hide()
 	end)
 
 	self:ApplyLook(popup, self.db.profile.style)
