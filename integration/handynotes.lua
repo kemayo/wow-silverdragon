c7d7af58091809fd9bfd322360961e3933156269
David Lynch <kemayo@gmail.com>
2018-07-21 21:28:15 -0500

    Wrong tooltip on world map

--- integration/handynotes.lua
+++ integration/handynotes.lua
@@ -92,7 +92,7 @@ do
 end
 
 function handler:OnEnter(uiMapID, coord)
-	local tooltip = self:GetParent() == WorldMapButton and WorldMapTooltip or GameTooltip
+	local tooltip = self:GetParent() == WorldMapFrame:GetCanvas() and WorldMapTooltip or GameTooltip
 	if self:GetCenter() > UIParent:GetCenter() then -- compare X coordinate
 		tooltip:SetOwner(self, "ANCHOR_LEFT")
 	else
@@ -122,7 +122,7 @@ function handler:OnEnter(uiMapID, coord)
 end
 
 function handler:OnLeave(uiMapID, coord)
-	if self:GetParent() == WorldMapButton then
+	if self:GetParent() == WorldMapFrame:GetCanvas() then
 		WorldMapTooltip:Hide()
 	else
 		GameTooltip:Hide()
