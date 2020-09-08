4e8536a5227998cca7cfe1ca499d7e96c59656dd
David Lynch <kemayo@gmail.com>
2020-09-08 18:35:09 -0500

    Generalize "tomtom" to "waypoints" to also make new UiMapPoints
    Refs #51

--- integration/tomtom.lua
+++ integration/tomtom.lua
@@ -10,6 +10,8 @@ function module:OnInitialize()
 		profile = {
 			enabled = true,
 			duration = 120,
+			tomtom = true,
+			replace = false,
 		},
 	})
 	db = self.db.profile
@@ -19,12 +21,14 @@ function module:OnInitialize()
 		config.options.plugins.tomtom = {
 			tomtom = {
 				type = "group",
-				name = "TomTom",
+				name = "Waypoints",
 				get = function(info) return self.db.profile[info[#info]] end,
 				set = function(info, v) self.db.profile[info[#info]] = v end,
 				args = {
-					about = config.desc("When we see a mob via its minimap icon, we can ask TomTom to point us to it", 0),
-					enabled = config.toggle("Point to it", "Tell TomTom about the mob", 30),
+					about = config.desc("When we see a mob via its minimap icon, we can ask an arrow to point us to it", 0),
+					enabled = config.toggle("Point to it", "Make a waypoint for the mob", 20),
+					tomtom = config.toggle("Use TomTom", "If TomTom is installed, use it", 25),
+					replace = config.toggle("Replace waypoints", "Replace an existing waypoint if one is set (doesn't apply to TomTom)", 30),
 					duration = {
 						type = "range",
 						name = "Duration",
@@ -45,26 +49,43 @@ end
 do
 	local waypoint
 	function module:Announce(_, id, zone, x, y, is_dead, source, unit)
-		if not TomTom then return end
 		if not db.enabled then return end
 		if source ~= "vignette" then return end
-		if waypoint then
-			TomTom:RemoveWaypoint(waypoint)
-		end
-		waypoint = TomTom:AddWaypoint(zone, x, y, {
-			title = core:GetMobLabel(id) or UNKNOWN,
-			persistent = false,
-			minimap = false,
-			world = false,
-			cleardistance = 25
-		})
-		waypoint.mobid = id
-		if db.duration and db.duration > 0 then
-			C_Timer.After(db.duration, function()
-				if waypoint and waypoint.mobid == id then
-					TomTom:RemoveWaypoint(waypoint)
+		if TomTom then
+			if waypoint then
+				TomTom:RemoveWaypoint(waypoint)
+			end
+			waypoint = TomTom:AddWaypoint(zone, x, y, {
+				title = core:GetMobLabel(id) or UNKNOWN,
+				persistent = false,
+				minimap = false,
+				world = false,
+				cleardistance = 25
+			})
+			waypoint.mobid = id
+			if db.duration and db.duration > 0 then
+				C_Timer.After(db.duration, function()
+					if waypoint and waypoint.mobid == id then
+						TomTom:RemoveWaypoint(waypoint)
+					end
+				end)
+			end
+		else
+			local uiMapPoint = UiMapPoint.CreateFromCoordinates(zone, x, y)
+			local current = C_Map.GetUserWaypoint()
+			if (not current) or db.replace then
+				C_Map.SetUserWaypoint(uiMapPoint)
+				C_SuperTrack.SetSuperTrackedUserWaypoint(true)
+				waypoint = uiMapPoint
+				if db.duration and db.duration > 0 then
+					C_Timer.After(db.duration, function()
+						local current = C_Map.GetUserWaypoint()
+						if current and waypoint and waypoint.position and waypoint.position:IsEqualTo(current.position) then
+							C_Map.ClearUserWaypoint()
+						end
+					end)
 				end
-			end)
+			end
 		end
 	end
 end
