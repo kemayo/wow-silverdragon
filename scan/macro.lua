9ff55663a7c0adbb3c4945c09056a987347b94b4
David Lynch <kemayo@gmail.com>
2020-10-09 11:32:25 -0500

    Macro improvements
    * Macro building should respect more ignore conditions
    * Chunk scanning macro so we don't get truncated in big zones

--- scan/macro.lua
+++ scan/macro.lua
@@ -3,6 +3,7 @@ local myname, ns = ...
 local core = LibStub("AceAddon-3.0"):GetAddon("SilverDragon")
 local module = core:NewModule("Macro", "AceEvent-3.0", "AceConsole-3.0")
 local Debug = core.Debug
+local DebugF = core.DebugF
 
 local HBD = LibStub("HereBeDragons-2.0")
 
@@ -17,6 +18,8 @@ function module:OnInitialize()
 	HBD.RegisterCallback(self, "PlayerZoneChanged", "Update")
 	core.RegisterCallback(self, "Seen", "Update")
 	core.RegisterCallback(self, "Ready", "Update")
+	core.RegisterCallback(self, "IgnoreChanged", "Update")
+	core.RegisterCallback(self, "CustomChanged", "Update")
 
 	local config = core:GetModule("Config", true)
 	if config then
@@ -54,27 +57,27 @@ function module:OnInitialize()
 	end
 end
 
+local macro = {}
 function module:Update()
 	if InCombatLockdown() then
 		self.waiting = true
 		return
 	end
 	if not self.db.profile.enabled then
-		self.button:SetAttribute("macrotext", "/print \"Scanning macro disabled\"")
+		self:GetMacroButton(1):SetAttribute("macrotext", "/print \"Scanning macro disabled\"")
 		return
 	end
 	Debug("Updating Macro")
 	-- first, create the macro text on the button:
 	local zone = HBD:GetPlayerZone()
 	local mobs = zone and ns.mobsByZone[zone]
-	local macro = {}
 	local count = 0
 	if mobs then
 		for id in pairs(mobs) do
 			local name = core:NameForMob(id)
 			if
 				name and
-				not core.db.global.ignore[id] and
+				not core:ShouldIgnoreMob(id, zone) and
 				core:IsMobInPhase(id, zone)
 			then
 				table.insert(macro, "/targetexact " .. name)
@@ -84,11 +87,28 @@ function module:Update()
 	end
 	if count == 0 then
 		table.insert(macro, "/print \"No mobs known to scan for\"")
-	end
-	if self.db.profile.verbose then
+	elseif self.db.profile.verbose then
 		table.insert(macro, 1, ("/print \"Scanning for %d nearby mobs...\""):format(count))
 	end
-	self.button:SetAttribute("macrotext", ("\n"):join(unpack(macro)))
+	local len = 0
+	local n = 1
+	local start = 1
+	for i, text in ipairs(macro) do
+		len = len + #text + 1 -- for the newline
+		local next_statement = macro[next(macro, i)]
+		if len > (255 - (math.max(31, #(next_statement or "")))) or not next_statement then -- for the length of the /click
+			local button = self:GetMacroButton(n)
+			n = n + 1
+			local mtext = ("\n"):join(unpack(macro, start, i))
+			if next_statement then
+				mtext = mtext .. "\n/click SilverDragonMacroButton"..n
+			end
+			button:SetAttribute("macrotext", mtext)
+			len = 0
+			start = i
+		end
+	end
+	DebugF("Updated macro: %d mobs, %d statements, %d buttons", count, #macro, n - 1)
 	table.wipe(macro)
 end
 
@@ -96,7 +116,6 @@ function module:CreateMacro()
 	if InCombatLockdown() then
 		return self:Print("|cffff0000Can't make a macro while in combat!|r")
 	end
-	LoadAddOn("Blizzard_MacroUI") -- required for MAX_ACCOUNT_MACROS
 	local macroIndex = GetMacroIndexByName("SilverDragon")
 	if macroIndex == 0 then
 		local numglobal,numperchar = GetNumMacros()
@@ -119,9 +138,17 @@ function module:PLAYER_REGEN_ENABLED()
 	end
 end
 
-local button = CreateFrame("Button", "SilverDragonMacroButton", nil, "SecureActionButtonTemplate")
-button:SetAttribute("type", "macro")
-button:SetAttribute("macrotext", "/script DEFAULT_CHAT_FRAME:AddMessage('SilverDragon Macro: Not initialized yet.', 1, 0, 0)")
-module.button = button
-
--- /spew SilverDragonMacroButton:GetAttribute("macrotext")
+-- /dump SilverDragonMacroButton:GetAttribute("macrotext")
+function module:GetMacroButton(i)
+	local name = "SilverDragonMacroButton"
+	if i > 1 then
+		name = name .. i
+	end
+	if _G[name] then
+		return _G[name]
+	end
+	local button = CreateFrame("Button", name, nil, "SecureActionButtonTemplate")
+	button:SetAttribute("type", "macro")
+	button:SetAttribute("macrotext", "/script DEFAULT_CHAT_FRAME:AddMessage('SilverDragon Macro: Not initialized yet.', 1, 0, 0)")
+	return button
+end
