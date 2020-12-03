01425f4f7f3ff3cc623cc78037ad68b9957c7231
David Lynch <kemayo@gmail.com>
2020-12-02 21:51:49 -0600

    Tweak the tooltip updater so I can force not showing something

--- outputs/tooltip.lua
+++ outputs/tooltip.lua
@@ -50,15 +50,15 @@ function module:UpdateTooltip(id, force_achievement, force_drop, force_id)
 		return
 	end
 
-	if self.db.profile.achievement or force_achievement then
+	if self.db.profile.achievement or force_achievement == true and not force_achievement == false then
 		ns:UpdateTooltipWithCompletion(GameTooltip, id)
 	end
 
-	if self.db.profile.drop or force_drop then
+	if self.db.profile.drop or force_drop == true and not force_drop == false then
 		ns.Loot.Summary.UpdateTooltip(GameTooltip, id)
 	end
 
-	if self.db.profile.id or force_id then
+	if self.db.profile.id or force_id and not force_id == false then
 		GameTooltip:AddDoubleLine("id", id, 1, 1, 0, 1, 1, 0)
 	end
 
