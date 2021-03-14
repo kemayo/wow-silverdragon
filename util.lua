kemayo/wow-silverdragon

commit 7b27d5c301c51a956550aa086296e5b03f09584e

Split out some utility functions

util.lua
@@ -1,0 +1,178 @@
+local myname, ns = ...
+
+local addon = LibStub("AceAddon-3.0"):GetAddon("SilverDragon")
+local Debug = addon.Debug
+local DebugF = addon.DebugF
+
+local HBD = LibStub("HereBeDragons-2.0")
+
+-- GUID / unit
+
+do
+	local valid_unit_types = {
+		Creature = true, -- npcs
+		Vehicle = true, -- vehicles
+	}
+	local function npcIdFromGuid(guid)
+		if not guid then return end
+		local unit_type, id = guid:match("(%a+)-%d+-%d+-%d+-%d+-(%d+)-.+")
+		if not (unit_type and valid_unit_types[unit_type]) then
+			return
+		end
+		return tonumber(id)
+	end
+	ns.IdFromGuid = npcIdFromGuid
+	function addon:UnitID(unit)
+		return npcIdFromGuid(UnitGUID(unit))
+	end
+	function addon:FindUnitWithID(id)
+		if self:UnitID('target') == id then
+			return 'target'
+		end
+		if self:UnitID('mouseover') == id then
+			return 'mouseover'
+		end
+		for _, nameplate in ipairs(C_NamePlate.GetNamePlates()) do
+			if self:UnitID(nameplate.namePlateUnitToken) == id then
+				return nameplate.namePlateUnitToken
+			end
+		end
+		if IsInGroup() then
+			local prefix = IsInRaid() and 'raid' or 'party'
+			for i=1, GetNumGroupMembers() do
+				local unit = prefix .. i .. 'target'
+				if self:UnitID(unit) == id then
+					return unit
+				end
+			end
+		end
+	end
+end
+
+addon.round = function(num, precision)
+	return math.floor(num * math.pow(10, precision) + 0.5) / math.pow(10, precision)
+end
+
+function addon:FormatLastSeen(t)
+	t = tonumber(t)
+	if not t or t == 0 then return NEVER end
+	local currentTime = time()
+	local minutes = math.floor(((currentTime - t) / 60) + 0.5)
+	if minutes > 119 then
+		local hours = math.floor(((currentTime - t) / 3600) + 0.5)
+		if hours > 23 then
+			return math.floor(((currentTime - t) / 86400) + 0.5).." day(s)"
+		else
+			return hours.." hour(s)"
+		end
+	else
+		return minutes.." minute(s)"
+	end
+end
+
+addon.zone_names = setmetatable({}, {__index = function(self, mapid)
+	if not mapid then
+		return
+	end
+	local mapdata = C_Map.GetMapInfo(mapid)
+	if mapdata then
+		self[mapid] = mapdata.name
+		return mapdata.name
+	end
+end,})
+
+-- Names
+
+do
+	local mobNameToId = {}
+
+	local cache_tooltip = CreateFrame("GameTooltip", "SDCacheTooltip", _G.UIParent, "GameTooltipTemplate")
+	cache_tooltip:SetOwner(_G.WorldFrame, "ANCHOR_NONE")
+	local function TextFromHyperlink(link)
+		cache_tooltip:ClearLines()
+		cache_tooltip:SetHyperlink(link)
+		local text = SDCacheTooltipTextLeft1:GetText()
+		if text and text ~= "" and text ~= UNKNOWN then
+			return text
+		end
+	end
+	function addon:NameForMob(id, unit)
+		if not self.db.locale.mob_name[id] then
+			local name = unit and UnitName(unit) or TextFromHyperlink(("unit:Creature-0-0-0-0-%d"):format(id))
+			if name and name ~= UNKNOWNOBJECT then
+				self.db.locale.mob_name[id] = name
+				mobNameToId[self.db.locale.mob_name[id]] = id
+			end
+		end
+		return self.db.locale.mob_name[id] or (ns.mobdb[id] and ns.mobdb[id].name)
+	end
+	function addon:IdForMob(name, zone)
+		if zone and ns.mobNamesByZone[zone] and ns.mobNamesByZone[zone][name] then
+			return ns.mobNamesByZone[zone][name]
+		end
+		return mobNameToId[name]
+	end
+end
+
+-- Location
+
+function addon:GetCoord(x, y)
+	return floor(x * 10000 + 0.5) * 10000 + floor(y * 10000 + 0.5)
+end
+
+function addon:GetXY(coord)
+	return floor(coord / 10000) / 10000, (coord % 10000) / 10000
+end
+
+function addon:GetClosestLocationForMob(id)
+	if not (ns.mobdb[id] and ns.mobdb[id].locations) then return end
+	local x, y, zone = HBD:GetPlayerZonePosition()
+	if not (x and y and zone) then return end
+	local closest = {distance = 999999999}
+	for zone2, coords in pairs(ns.mobdb[id].locations) do
+		for i, coord in ipairs(coords) do
+			local x2, y2 = self:GetXY(coord)
+			local distance = HBD:GetZoneDistance(zone, x, y, zone2, x2, y2)
+			if not distance then
+				if not closest.zone then
+					-- make sure we get one
+					closest.zone = zone2
+					closest.x = x2
+					closest.y = y2
+				end
+			end
+			if distance < closest.distance then
+				closest.distance = distance
+				closest.zone = zone2
+				closest.x = x2
+				closest.y = y2
+			end
+		end
+	end
+	return closest.zone, closest.x, closest.y, closest.distance
+end
+
+-- Tooltip stuff
+
+ns.Tooltip = {
+	Get = function(name)
+		name = "SilverDragon" .. name .. "Tooltip"
+		if _G[name] then
+			return _G[name]
+		end
+		local tooltip = CreateFrame("GameTooltip", name, UIParent, "GameTooltipTemplate")
+		tooltip:SetScript("OnTooltipSetUnit", GameTooltip_OnTooltipSetUnit)
+		tooltip:SetScript("OnTooltipSetItem", GameTooltip_OnTooltipSetItem)
+		tooltip:SetScript("OnTooltipSetSpell", GameTooltip_OnTooltipSetSpell)
+		tooltip:SetScript("OnUpdate", GameTooltip_OnUpdate)
+		tooltip.shoppingTooltips = {
+			CreateFrame("GameTooltip", name.."Shopping1", tooltip, "GameTooltipTemplate"),
+			CreateFrame("GameTooltip", name.."Shopping2", tooltip, "GameTooltipTemplate"),
+		}
+		tooltip.shoppingTooltips[1]:SetScript("OnTooltipSetItem", GameTooltip_OnTooltipSetShoppingItem)
+		tooltip.shoppingTooltips[1]:SetScale(0.8)
+		tooltip.shoppingTooltips[2]:SetScript("OnTooltipSetItem", GameTooltip_OnTooltipSetShoppingItem)
+		tooltip.shoppingTooltips[2]:SetScale(0.8)
+		return tooltip
+	end,
+}

.luacheckrc
@@ -56,12 +56,14 @@ read_globals = {
     "C_AreaPoiInfo",
     "C_ChatInfo",
     "C_Covenants",
+    "C_CurrencyInfo",
     "C_Item",
     "C_Map",
     "C_MountJournal",
     "C_NamePlate",
     "C_PetJournal",
     "C_QuestLog",
+    "C_Spell",
     "C_SuperTrack",
     "C_Texture",
     "C_Timer",
@@ -125,6 +127,7 @@ read_globals = {
     "GetRaidTargetIndex",
     "GetScreenHeight",
     "GetScreenWidth",
+    "GetSpellInfo",
     "GetTime",
     "GetZoneText",
     "InCombatLockdown",

SilverDragon.toc
@@ -27,6 +27,7 @@ achievements.lua
 loot.lua
 slash.lua
 config.lua
+util.lua
 
 scan\targets.lua
 scan\macro.lua

core.lua
@@ -42,12 +42,6 @@ do
 	Debug = addon.Debug
 end
 
-local mfloor, mpow, mabs = math.floor, math.pow, math.abs
-local tinsert, tremove = table.insert, table.remove
-local ipairs, pairs = ipairs, pairs
-local IsInInstance, GetCurrentMapAreaID, SetMapByID, SetMapToCurrentZone = IsInInstance, GetCurrentMapAreaID, SetMapByID, SetMapToCurrentZone
-local wowVersion, buildRevision, _, buildTOC = GetBuildInfo()
-
 BINDING_HEADER_SILVERDRAGON = "SilverDragon"
 _G["BINDING_NAME_CLICK SilverDragonPopupButton:LeftButton"] = "Target last found mob"
 _G["BINDING_NAME_CLICK SilverDragonMacroButton:LeftButton"] = "Scan for nearby mobs"
@@ -269,60 +263,6 @@ function addon:SetCustom(id, watch, quiet)
 	return true
 end
 
-do
-	local mobNameToId = {}
-	local questNameToId = {}
-
-	local cache_tooltip = CreateFrame("GameTooltip", "SDCacheTooltip", _G.UIParent, "GameTooltipTemplate")
-	cache_tooltip:SetOwner(_G.WorldFrame, "ANCHOR_NONE")
-	local function TextFromHyperlink(link)
-		cache_tooltip:ClearLines()
-		cache_tooltip:SetHyperlink(link)
-		local text = SDCacheTooltipTextLeft1:GetText()
-		if text and text ~= "" and text ~= UNKNOWN then
-			return text
-		end
-	end
-	function addon:NameForMob(id, unit)
-		if not self.db.locale.mob_name[id] then
-			local name = TextFromHyperlink(("unit:Creature-0-0-0-0-%d"):format(id))
-			if unit and not name then
-				name = UnitName(unit)
-			end
-			if name and name ~= UNKNOWNOBJECT then
-				self.db.locale.mob_name[id] = name
-			end
-		end
-		if self.db.locale.mob_name[id] then
-			mobNameToId[self.db.locale.mob_name[id]] = id
-		end
-		return self.db.locale.mob_name[id] or (mobdb[id] and mobdb[id].name)
-	end
-	function addon:IdForMob(name, zone)
-		if zone and mobNamesByZone[zone] and mobNamesByZone[zone][name] then
-			return mobNamesByZone[zone][name]
-		end
-		return mobNameToId[name]
-	end
-	function addon:NameForQuest(id)
-		if not self.db.locale.quest_name[id] then
-			-- TODO: after 9.0.1 this check can be removed
-			local name = C_QuestLog.GetTitleForQuestID and C_QuestLog.GetTitleForQuestID(id) or C_QuestLog.GetQuestInfo(id)
-			if name then
-				name = name:gsub("Vignette: ", "")
-				self.db.locale.quest_name[id] = name
-			end
-		end
-		if self.db.locale.quest_name[id] then
-			questNameToId[self.db.locale.quest_name[id]] = id
-		end
-		return self.db.locale.quest_name[id]
-	end
-	function addon:IdForQuest(name)
-		return questNameToId[name]
-	end
-end
-
 -- returns name, questid, vignette, tameable, last_seen, times_seen
 function addon:GetMobInfo(id)
 	if mobdb[id] then
@@ -483,141 +423,3 @@ function addon:CheckNearby()
 
 	self.events:Fire("Scan", zone)
 end
-
--- Utility:
-
-do
-	local valid_unit_types = {
-		Creature = true, -- npcs
-		Vehicle = true, -- vehicles
-	}
-	local function npcIdFromGuid(guid)
-		if not guid then return end
-		local unit_type, id = guid:match("(%a+)-%d+-%d+-%d+-%d+-(%d+)-.+")
-		if not (unit_type and valid_unit_types[unit_type]) then
-			return
-		end
-		return tonumber(id)
-	end
-	ns.IdFromGuid = npcIdFromGuid
-	function addon:UnitID(unit)
-		return npcIdFromGuid(UnitGUID(unit))
-	end
-	function addon:FindUnitWithID(id)
-		if self:UnitID('target') == id then
-			return 'target'
-		end
-		if self:UnitID('mouseover') == id then
-			return 'mouseover'
-		end
-		for _, nameplate in ipairs(C_NamePlate.GetNamePlates()) do
-			if self:UnitID(nameplate.namePlateUnitToken) == id then
-				return nameplate.namePlateUnitToken
-			end
-		end
-		if IsInGroup() then
-			local prefix = IsInRaid() and 'raid' or 'party'
-			for i=1, GetNumGroupMembers() do
-				local unit = prefix .. i .. 'target'
-				if self:UnitID(unit) == id then
-					return unit
-				end
-			end
-		end
-	end
-end
-
-addon.round = function(num, precision)
-	return mfloor(num * mpow(10, precision) + 0.5) / mpow(10, precision)
-end
-
-function addon:FormatLastSeen(t)
-	t = tonumber(t)
-	if not t or t == 0 then return NEVER end
-	local currentTime = time()
-	local minutes = mfloor(((currentTime - t) / 60) + 0.5)
-	if minutes > 119 then
-		local hours = mfloor(((currentTime - t) / 3600) + 0.5)
-		if hours > 23 then
-			return mfloor(((currentTime - t) / 86400) + 0.5).." day(s)"
-		else
-			return hours.." hour(s)"
-		end
-	else
-		return minutes.." minute(s)"
-	end
-end
-
-addon.zone_names = setmetatable({}, {__index = function(self, mapid)
-	if not mapid then
-		return
-	end
-	local mapdata = C_Map.GetMapInfo(mapid)
-	if mapdata then
-		self[mapid] = mapdata.name
-		return mapdata.name
-	end
-end,})
-
--- Location
-
-function addon:GetCoord(x, y)
-	return floor(x * 10000 + 0.5) * 10000 + floor(y * 10000 + 0.5)
-end
-
-function addon:GetXY(coord)
-	return floor(coord / 10000) / 10000, (coord % 10000) / 10000
-end
-
-function addon:GetClosestLocationForMob(id)
-	if not (ns.mobdb[id] and ns.mobdb[id].locations) then return end
-	local x, y, zone = HBD:GetPlayerZonePosition()
-	if not (x and y and zone) then return end
-	local closest = {distance = 999999999}
-	for zone2, coords in pairs(ns.mobdb[id].locations) do
-		for i, coord in ipairs(coords) do
-			local x2, y2 = self:GetXY(coord)
-			local distance = HBD:GetZoneDistance(zone, x, y, zone2, x2, y2)
-			if not distance then
-				if not closest.zone then
-					-- make sure we get one
-					closest.zone = zone2
-					closest.x = x2
-					closest.y = y2
-				end
-			end
-			if distance < closest.distance then
-				closest.distance = distance
-				closest.zone = zone2
-				closest.x = x2
-				closest.y = y2
-			end
-		end
-	end
-	return closest.zone, closest.x, closest.y, closest.distance
-end
-
--- utility tooltip stuff
-
-ns.Tooltip = {
-	Get = function(name)
-		name = "SilverDragon" .. name .. "Tooltip"
-		if _G[name] then
-			return _G[name]
-		end
-		local tooltip = CreateFrame("GameTooltip", name, UIParent, "GameTooltipTemplate")
-		tooltip:SetScript("OnTooltipSetUnit", GameTooltip_OnTooltipSetUnit)
-		tooltip:SetScript("OnTooltipSetItem", GameTooltip_OnTooltipSetItem)
-		tooltip:SetScript("OnTooltipSetSpell", GameTooltip_OnTooltipSetSpell)
-		tooltip:SetScript("OnUpdate", GameTooltip_OnUpdate)
-		tooltip.shoppingTooltips = {
-			CreateFrame("GameTooltip", name.."Shopping1", tooltip, "GameTooltipTemplate"),
-			CreateFrame("GameTooltip", name.."Shopping2", tooltip, "GameTooltipTemplate"),
-		}
-		tooltip.shoppingTooltips[1]:SetScript("OnTooltipSetItem", GameTooltip_OnTooltipSetShoppingItem)
-		tooltip.shoppingTooltips[1]:SetScale(0.8)
-		tooltip.shoppingTooltips[2]:SetScript("OnTooltipSetItem", GameTooltip_OnTooltipSetShoppingItem)
-		tooltip.shoppingTooltips[2]:SetScale(0.8)
-		return tooltip
-	end,
-}

loot.lua
@@ -665,8 +665,6 @@ do
 
 	ns.Loot.Window = {}
 
-	local loot_tooltip = ns.Tooltip.Get("Loot")
-
 	local function window_onclick(self, mousebutton)
 		if mousebutton == "RightButton" then
 			if self.independent then
@@ -677,6 +675,7 @@ do
 		end
 	end
 	local function button_onenter(self)
+		local loot_tooltip = ns.Tooltip.Get("Loot")
 		loot_tooltip:SetFrameStrata(self:GetFrameStrata())
 		loot_tooltip:SetFrameLevel(self:GetFrameLevel() + 1)
 		if self:GetCenter() > UIParent:GetCenter() then
@@ -693,7 +692,7 @@ do
 		self:GetParent().tooltip = loot_tooltip
 	end
 	local function button_onleave(self)
-		loot_tooltip:Hide()
+		ns.Tooltip.Get("Loot"):Hide()
 		self:GetParent().tooltip = nil
 	end
 	local function button_onclick(self, mousebutton)

scan/vignettes.lua
@@ -217,11 +217,6 @@ function module:WorkOutMobFromVignette(instanceid)
 			Debug("vignetteMobLookup", vignetteInfo.name, vignetteInfo.vignetteID, ns.vignetteMobLookup[vignetteInfo.name])
 			return self:NotifyForMobs(ns.vignetteMobLookup[vignetteInfo.name], current_zone, x, y, source, instanceid)
 		end
-		local questid = core:IdForQuest(vignetteInfo.name)
-		if questid and ns.questMobLookup[questid] then
-			Debug("questMobLookup", vignetteInfo.name, ns.questMobLookup[questid])
-			return self:NotifyForMobs(ns.questMobLookup[questid], current_zone, x, y, source, instanceid)
-		end
 		local mobid = core:IdForMob(vignetteInfo.name)
 		if mobid then
 			Debug("name", vignetteInfo.name, mobid)

commit de28a4537d1ecbd7d80b4b0c8d5064381bdf6102

Fix chat scanning
Broken because the refactor in 7b27d5c broke the name-to-id cache unless
it was the first time that mob's locale cache was filled.

Made worse because I'd never finished the by-zone name lookup in 270d215
apparently.

Fixes #120

util.lua
@@ -175,14 +175,27 @@ do
 			local name = unit and UnitName(unit) or TextFromHyperlink(("unit:Creature-0-0-0-0-%d"):format(id))
 			if name and name ~= UNKNOWNOBJECT then
 				self.db.locale.mob_name[id] = name
-				mobNameToId[self.db.locale.mob_name[id]] = id
 			end
+		end
+		if self.db.locale.mob_name[id] then
+			mobNameToId[self.db.locale.mob_name[id]] = id
 		end
 		return self.db.locale.mob_name[id] or (ns.mobdb[id] and ns.mobdb[id].name)
 	end
 	function addon:IdForMob(name, zone)
-		if zone and ns.mobNamesByZone[zone] and ns.mobNamesByZone[zone][name] then
-			return ns.mobNamesByZone[zone][name]
+		if zone and ns.mobsByZone[zone] then
+			if not ns.mobNamesByZone[zone] then
+				ns.mobNamesByZone[zone] = {}
+				for id in pairs(ns.mobsByZone[zone]) do
+					local name = addon:NameForMob(id)
+					if name then
+						ns.mobNamesByZone[zone][name] = id
+					end
+				end
+			end
+			if ns.mobNamesByZone[zone][name] then
+				return ns.mobNamesByZone[zone][name]
+			end
 		end
 		return mobNameToId[name]
 	end