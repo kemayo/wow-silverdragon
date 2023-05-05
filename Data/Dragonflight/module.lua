1e682c369e70997ebeae1e03b5e6a85af3c8c190
David Lynch <kemayo@gmail.com>
2023-05-04 21:35:56 -0500

    First data for Zaralek from HandyNotes (incomplete)

--- Data/Dragonflight/module.lua
+++ Data/Dragonflight/module.lua
@@ -12,11 +12,13 @@ local THALDRASZUS = 2025
 local FORBIDDENREACH = 2151 -- 2026
 local FORBIDDENREACHINTRO = 2118 -- Dracthyr
 local PRIMALISTFUTURE = 2085
+local ZARALEKCAVERN = 2133
 
 local FACTION_MARUUK = 2503
 local FACTION_DRAGONSCALE = 2507
 local FACTION_VALDRAKKEN = 2510
 local FACTION_ISKAARA = 2511
+local FACTION_LOAMM = 2564
 
 -- Rares
 
@@ -186,10 +188,12 @@ local ns = {
 	FORBIDDENREACHINTRO = FORBIDDENREACHINTRO,
 	FORBIDDENREACH = FORBIDDENREACH,
 	PRIMALISTFUTURE = PRIMALISTFUTURE,
+	ZARALEKCAVERN = ZARALEKCAVERN,
 	FACTION_MARUUK = FACTION_MARUUK,
 	FACTION_DRAGONSCALE = FACTION_DRAGONSCALE,
 	FACTION_VALDRAKKEN = FACTION_VALDRAKKEN,
 	FACTION_ISKAARA = FACTION_ISKAARA,
+	FACTION_LOAMM = FACTION_LOAMM,
 	MAXLEVEL = {core.conditions.QuestComplete(67030), core.conditions.Level(70)},
 	DRAGONRIDING = core.conditions.SpellKnown(376777),
 	SUPERRARE = function(point)
@@ -3226,4 +3230,143 @@ ns.RegisterPoints(ns.FORBIDDENREACH, {
 	},
 }, {
 	achievement=17525, -- Champion of the Forbidden Reach
+})
+
+ns.RegisterPoints(ns.ZARALEKCAVERN, {
+	[48367509] = { -- Aquifon
+		criteria=59185,
+		quest=75270, -- 75271
+		npc=203468,
+		loot={205295}, -- Sediment Sifters
+		vignette=5640,
+	},
+	[57786911] = { -- Underlight Queen
+		criteria=59191,
+		quest=nil,
+		npc=203593,
+		vignette=5646,
+	},
+	[41518613] = { -- Brullo the Strong (Brulsef the Stronk?)
+		criteria=59202,
+		quest=75325, -- 75326
+		npc=203621,
+		vignette=5652,
+	},
+	[55841899] = { -- Professor Gastrinax
+		criteria=59189,
+		quest=nil,
+		npc=203521,
+		vignette=5644,
+	},
+	[46103346] = { -- Invoq (Invohq?)
+		criteria=59200,
+		quest=nil,
+		npc=203627,
+		vignette=5654,
+	},
+	[28875085] = { -- Dinn (Jrumm?)
+		criteria=59206,
+		quest=nil,
+		npc=203646,
+		vignette=5660,
+	},
+	[36324481] = { -- Flowfy
+		criteria=59207,
+		quest=nil,
+		npc=203660,
+		vignette=5661,
+	},
+	[47822342] = { -- Colossian
+		criteria=59212,
+		quest=nil,
+		npc=204093,
+		vignette=5674,
+	},
+	[39407061] = { -- Viridian King
+		criteria=59210,
+		quest=75366,
+		npc=201029,
+		vignette=5664,
+	},
+	[60293933] = { -- Kapraku (Kaprachu?)
+		criteria=59184,
+		quest=75269,
+		npc=203466,
+		loot={
+			205319, -- Deepflayer Shoulderguards
+			205310, -- Legguards of Kaprachu
+		},
+		vignette=5639,
+	},
+	[42521879] = { -- General Zskorro
+		criteria=59190,
+		quest=75296,
+		npc=203592,
+		loot={205321}, -- Brimstone Bracers
+		vignette=5645,
+	},
+	[32445127] = { -- Emberdusk
+		criteria=59209,
+		quest=75364,
+		npc=203664,
+		loot={205335}, -- Talisman of the Dusk
+		vignette=5663,
+	},
+	[65435587] = { -- Kob'rok
+		criteria=59183,
+		quest=75267,
+		npc=203462,
+		loot={
+			{206021, pet=3545}, -- Kob'rok's Luminescent Scale
+			{205152, pet=3546}, -- Skaarn
+			{205147, pet=3541}, -- Ridged Shalewing
+			205307, -- Kob'rok's Scale Sabatons
+		},
+		vignette=5638,
+	},
+	[68734593] = { -- Goopal
+		criteria=59186,
+		quest=75273, -- 75274
+		npc=203477,
+		vignette=5641,
+	},
+	[42176584] = { -- Karokta
+		criteria=59199,
+		quest=75334,
+		npc=203625,
+		vignette=5653,
+	},
+	[53604120] = { -- Klakatak
+		criteria=59198,
+		quest=75322,
+		npc=203618,
+		loot={205686}, -- Clacking Claw
+		vignette=5651,
+	},
+	[36425329] = { -- Skornak
+		criteria=59205,
+		quest=nil,
+		npc=203643,
+		vignette=5659,
+	},
+	[53106421] = { -- Spinmarrow
+		criteria=59187,
+		quest=nil,
+		npc=203480,
+		path=54986573,
+	},
+	[37954642] = { -- Subterrax
+		criteria=59208,
+		quest=75360,
+		npc=203662,
+		vignette=5662,
+	},
+	[41383744] = { -- Magtembo (magmanesha?)
+		criteria=59203,
+		quest=75339, -- 75340
+		npc=200111,
+		vignette=5656,
+	},
+}, {
+	achievement=17783, -- Adventurer of Zaralek Cavern
 })
