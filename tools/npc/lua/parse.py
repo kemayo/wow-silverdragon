1d28f7b0973284f583d2ca5fea09e3f50644296e
David Lynch <kemayo@gmail.com>
2023-07-01 13:59:27 -0500

    Extend my lua parser a bit further

--- tools/npc/lua/parse.py
+++ tools/npc/lua/parse.py
@@ -6,20 +6,43 @@ import ply.lex as lex
 Important reference: http://www.lua.org/manual/5.1/manual.html#8
 
 This is incomplete. It parses enough of Lua's syntax to handle non-fancy
-tables. In the official grammar provided in the manual, it starts at
-"tableconstructor". It has no support for functions, or for complicated
-expressions as table values. (e.g. `t[var]` will just error.)
+tables. In the official grammar provided in the manual, it uses "exp" as its
+entry point but doesn't handle function definitions. It has limited support for
+complicated expressions, but doesn't attempt to make sense of them.
 
 """
 
 
 class Literal:
+    """This exists to wrap content that's not really handled by this parser
+    but which should be preserved literally for output
+
+    e.g. it wraps around all variable names
+    """
     def __init__(self, content):
         self.content = [content]
     def __repr__(self):
         return ''.join(self.content)
     def extend(self, content):
         self.content.extend(content)
+
+
+class BinOp:
+    def __init__(self, left, op, right):
+        self.left = left
+        self.right = right
+        self.op = op
+    def __repr__(self):
+        # repr is needed on the values to preserve strings
+        return f'{repr(self.left)} {self.op} {repr(self.right)}'
+
+
+class UnOp:
+    def __init__(self, op, exp):
+        self.exp = exp
+        self.op = op
+    def __repr__(self):
+        return f'{self.op} {repr(self.exp)}'
 
 
 class FunctionCall:
@@ -32,7 +55,18 @@ class FunctionCall:
 
 # Lexer
 
-tokens = (
+reserved = {
+    # "if" : "IF",
+    # "then" : "THEN",
+    # "else" : "ELSE",
+    # "while" : "WHILE",
+    # "for" : "FOR",
+    # "return" : "RETURN"
+    "and" : "AND",
+    "or" : "OR",
+    "not" : "NOT",
+}
+tokens = [
     "OPBRACE",
     "CLBRACE",
     "OPBRAK",
@@ -50,15 +84,24 @@ tokens = (
     "BOOL",
     "NIL",
     "NEWLINE",
-)
+    "COMPARISON",
+    "OPERATOR",
+    "OPERATOR_MINUS",
+    "DOTDOT",
+    "HASH",
+] + list(reserved.values())
 t_OPBRACE = r"{"
 t_CLBRACE = r"}"
 t_OPBRAK = r"\["
 t_CLBRAK = r"\]"
 t_OPPAREN = r"\("
 t_CLPAREN = r"\)"
-t_NAME = r"[A-Za-z_][A-Za-z_0-9]*"
+t_COMPARISON = r"(?:<=|<|>=|>|==|~=)"
+t_OPERATOR = r"(?:\+|\*|\^|%)"
+t_OPERATOR_MINUS = r"-"
+t_HASH = r"\#"
 t_EQUALS = r"="
+t_DOTDOT = r"\.\."
 t_DOT = r"\."
 t_COMMA = r","
 t_COLON = r":"
@@ -66,6 +109,11 @@ t_SEMICOLON = r";"
 
 t_ignore = r" "
 
+
+def t_NAME(t):
+    r"[A-Za-z_][A-Za-z_0-9]*"
+    t.type = reserved.get(t.value, 'NAME')  # check for reserved words
+    return t
 
 # a string is quotes around a sequence of anything that's not-quotes-or-backslashes, or backslash+char
 def t_STRING(t):
@@ -121,11 +169,37 @@ def p_exp(p):
            | STRING
            | tableconstructor
            | prefixexp
+           | exp binop exp
+           | unop exp
     """
     # Note: incomplete, both in accepted values and in handling-of-values.
-    # Most importantly: prefixexp isn't handled, so this deals solely with
-    # literals and really simple variable references.
-    p[0] = p[1]
+    if len(p) == 3:
+        # unop exp
+        p[0] = UnOp(p[1], p[2])
+    elif len(p) == 4:
+        # exp binop exp
+        p[0] = BinOp(p[1], p[2], p[3])
+    else:
+        p[0] = p[1]
+
+
+def p_binop(p):
+    """binop : OPERATOR
+             | OPERATOR_MINUS
+             | DOTDOT
+             | COMPARISON
+             | AND
+             | OR
+    """
+    p[0] = Literal(p[1])
+
+
+def p_unop(p):
+    """unop : OPERATOR_MINUS
+            | NOT
+            | HASH
+    """
+    p[0] = Literal(p[1])
 
 
 def p_tableconstructor(p):
@@ -201,12 +275,19 @@ def p_field(p):
 
 
 def p_var(p):
-    """var : var DOT NAME
+    """var : prefixexp DOT NAME
+           | prefixexp OPBRAK exp CLBRAK
            | NAME
     """
+    # print(p[1], len(p) > 2 and p[3])
     if len(p) == 2:
+        # `name`
         p[0] = Literal(p[1])
-    else:
+    elif p[2] == "[":
+        # `prefix[exp]`
+        p[0] = Literal(f"{p[1]}[{p[3]}]")
+    elif p[2] == ".":
+        # `prefix.name`
         p[1].extend(p[2:])
         p[0] = p[1]
 
@@ -270,6 +351,7 @@ def parse(s, *args, **kwargs):
     parser.error = 0
     p = parser.parse(s, *args, **kwargs)
     if parser.error:
+        # print(parser.error)
         return None
     return p
 
@@ -277,7 +359,7 @@ def parse(s, *args, **kwargs):
 if __name__ == "__main__":
     # s = '{23.4, "pony express\\" ri\'de", \'Test\\\'s fun\', 4, "apple", fred=400, ["foo"]=999, [90]="beauty", {1,2}, p={2},}'
     # s = r'{[61] = {name="Thuros \"Fred\" Lightfingers",["creature_type"]="Humanoid",level=9,locations={[30]={50408320,50408280},},},loot={},test=Enum.foo.bar}'
-    s = r'{a=b.c.d, q={}, qq={1,2,3}, x=aa.y:frog{z, zz, "hi", {a=b()}, t=22}}'
+    s = r'{a=b.c.d, c[1], d[-e], q={}, qq={1,2,-3}, x=aa.y:frog{z, not zz, not not zzz, "hi".."bye", {a=b(), s=s(1,2, a>=4)}, t=22}}'
     print(s)
 
     lexer.input(s)
