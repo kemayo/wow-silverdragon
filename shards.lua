a490ae6f313d192a655cea76a9827b8288e467fc
David Lynch <kemayo@gmail.com>
2024-08-05 23:58:45 -0500

    Add some tracking of the current shard

---
+++ shards.lua
@@ -0,0 +1,52 @@
+local myname, ns = ...
+
+local core = LibStub("AceAddon-3.0"):GetAddon("SilverDragon")
+local module = core:NewModule("Shards", "AceConsole-3.0", "AceEvent-3.0")
+local Debug = core.Debug
+
+function module:OnEnable()
+	self:RegisterEvent("PLAYER_TARGET_CHANGED")
+	self:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
+	self:RegisterEvent("NAME_PLATE_UNIT_ADDED")
+	self:RegisterEvent("VIGNETTE_MINIMAP_UPDATED")
+	self:RegisterEvent("VIGNETTES_UPDATED")
+	-- todo: combat log as well?
+
+	-- Can't know it until we see an event
+	self.currentShard = "unknown"
+end
+
+function module:PLAYER_TARGET_CHANGED()
+	self:TrackCurrentShard(UnitGUID("target"))
+end
+function module:UPDATE_MOUSEOVER_UNIT()
+	self:TrackCurrentShard(UnitGUID("mouseover"))
+end
+function module:NAME_PLATE_UNIT_ADDED(event, unit)
+	self:TrackCurrentShard(UnitGUID(unit))
+end
+function module:VIGNETTE_MINIMAP_UPDATED(event, guid)
+	self:TrackCurrentShard(guid)
+end
+function module:VIGNETTES_UPDATED()
+	local vignetteids = C_VignetteInfo.GetVignettes()
+	for i=1, #vignetteids do
+		self:TrackCurrentShard(vignetteids[i])
+	end
+end
+
+function module:TrackCurrentShard(guid)
+	if not guid then return end
+	local shard = core:GUIDShard(guid)
+	if shard and self.currentShard ~= shard then
+		local lastShard = self.currentShard
+		self.currentShard = shard
+		Debug("ShardChanged", self.currentShard, lastShard, guid)
+		core.events:Fire("ShardChanged", self.currentShard, lastShard, guid)
+		return true
+	end
+end
+
+function module:GetCurrentShard()
+	return self.currentShard
+end
--- util.lua
+++ util.lua
@@ -140,6 +140,10 @@ do
 	end
 end
 
+function addon:ColorTextByCompleteness(complete, text)
+	return (complete and completeColor or incompleteColor):WrapTextInColorCode(text)
+end
+
 -- GUID / unit
 
 do
@@ -180,6 +184,24 @@ do
 				end
 			end
 		end
+	end
+end
+do
+	local valid_types = {
+		Creature = true, -- npcs
+		Vehicle = true, -- vehicles
+		Vignette = true, -- vignettes ()
+	}
+	-- See: https://warcraft.wiki.gg/wiki/GUID#Creature
+	function addon:GUIDShard(guid)
+		if not guid then return end
+		-- local unitType, _, serverID, instanceID, zoneUID, mobID, spawnUID = strsplit("-", guid)
+		local guidType, _, serverID, instanceID, zoneUID, id, spawnUID = strsplit("-", guid)
+		if not (guidType and valid_types[guidType]) then return end
+		return tonumber(zoneUID)
+	end
+	function addon:UnitShard(unit)
+		return self:GUIDShard(UnitGUID(unit))
 	end
 end
 
