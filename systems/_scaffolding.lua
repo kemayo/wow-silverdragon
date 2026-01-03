7d25fd7ae2000b77fb0f3c946af047d5e9718441
David Lynch <kemayo@gmail.com>
2026-01-03 11:50:05 -0600

    Reward system scaffolding missing data

--- systems/_scaffolding.lua
+++ systems/_scaffolding.lua
@@ -17,3 +17,26 @@ ns.ClearRunCaches = function()
 		table.wipe(cache)
 	end
 end
+
+local playerClassLocal, playerClass = UnitClass("player")
+ns.playerClass = playerClass
+ns.playerClassLocal = playerClassLocal
+ns.playerClassColor = RAID_CLASS_COLORS[playerClass]
+ns.playerName = UnitName("player")
+ns.playerFaction = UnitFactionGroup("player")
+ns.playerClassMask = ({
+    -- this is 2^(classID - 1)
+    WARRIOR = 0x1,
+    PALADIN = 0x2,
+    HUNTER = 0x4,
+    ROGUE = 0x8,
+    PRIEST = 0x10,
+    DEATHKNIGHT = 0x20,
+    SHAMAN = 0x40,
+    MAGE = 0x80,
+    WARLOCK = 0x100,
+    MONK = 0x200,
+    DRUID = 0x400,
+    DEMONHUNTER = 0x800,
+    EVOKER = 0x1000,
+})[playerClass] or 0
