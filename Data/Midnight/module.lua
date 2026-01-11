e60d59333ecea7e6e3ba9c0cf0bdcf75080709ba
David Lynch <kemayo@gmail.com>
2026-01-11 16:44:20 -0600

    Midnight: world boss loot in Eversong, Zul'Aman, Harandar

--- Data/Midnight/module.lua
+++ Data/Midnight/module.lua
@@ -216,6 +216,58 @@ core:RegisterTreasureData("Midnight", {
 
 -- Rares
 
+core:RegisterMobData("Midnight", {
+	-- World bosses
+	[244762] = {
+		name="Lu'ashal",
+		quest=92560,
+		worldquest=92560,
+		locations={[ns.EVERSONGWOODS]={}}, -- 45245997
+		loot={
+			250447, -- Radiant Eversong Scepter
+			250451, -- Dawncrazed Beast Cleaver
+			250453, -- Scepter of the Unbound Light
+			250456, -- Wretched Scholar's Gilded Robe
+			250457, -- Devouring Outrider's Chausses
+			250458, -- Host Commander's Casque
+			250459, -- Bramblestalker's Feathered Cowl
+			250462, -- Forgotten Farstrider's Insignia
+		},
+	},
+	[244424] = {
+		name="Cragpine",
+		quest=92123,
+		worldquest=92123,
+		locations={[ns.ZULAMAN]={}}, -- 45244790
+		loot={
+			250446, -- Cragtender Bulwark
+			250450, -- Forest Sentinel's Savage Longbow
+			250456, -- Wretched Scholar's Gilded Robe
+			250457, -- Devouring Outrider's Chausses
+			250458, -- Host Commander's Casque
+			250459, -- Bramblestalker's Feathered Cowl
+			250461, -- Chain of the Ancient Watcher
+			250462, -- Forgotten Farstrider's Insignia
+		},
+	},
+	[249776] = {
+		name="Thorm'belan",
+		quest=92034,
+		worldquest=92034,
+		locations={[ns.HARANDAR]={}}, -- 39026691
+		loot={
+			250449, -- Skulking Nettledirk
+			250452, -- Blooming Thornblade
+			250455, -- Beastly Blossombarb
+			250456, -- Wretched Scholar's Gilded Robe
+			250457, -- Devouring Outrider's Chausses
+			250458, -- Host Commander's Casque
+			250459, -- Bramblestalker's Feathered Cowl
+			250462, -- Forgotten Farstrider's Insignia
+		},
+	},
+}, true)
+
 ns.RegisterPoints(ns.ISLEOFQUELDANAS, {
 	[55712913] = {
 		label="Tarhu the Ransacker",
