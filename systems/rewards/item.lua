ed3147f6ef22bafeb579cf8b42793e24597e0a6a
David Lynch <kemayo@gmail.com>
2026-02-28 10:09:30 -0600

    Item system: one more large gold fix

--- systems/rewards/item.lua
+++ systems/rewards/item.lua
@@ -85,7 +85,7 @@ do
 		local G, S, C = GOLD_AMOUNT_SYMBOL, SILVER_AMOUNT_SYMBOL, COPPER_AMOUNT_SYMBOL
 		if c >= 10000 then
 			return ("|cffffffff%s|r|cffffd700%s|r |cffffffff%d|r|cffc7c7cf%s|r |cffffffff%d|r|cffeda55f%s|r"):format(
-				BreakUpLargeNumbers(c/10000), G, (c/100)%100, S, c%100, C
+				BreakUpLargeNumbers(math.floor(c/10000)), G, math.floor(c/100)%100, S, c%100, C
 			)
 		elseif c >= 100 then
 			return ("|cffffffff%d|r|cffc7c7cf%s|r |cffffffff%d|r|cffeda55f%s|r"):format((c/100)%100, S, c%100, C)
