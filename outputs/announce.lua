a83e6b5939b164396c609f0a45ed4cb5ffcab6d0
David Lynch <kemayo@gmail.com>
2026-08-17 11:08:08 -0500

    New sound: Kobold saying "It's not junk, it's treasure..."
    Per suggestion by warchild1x

--- outputs/announce.lua
+++ outputs/announce.lua
@@ -35,6 +35,7 @@ LSM:Register("sound", "Illidan: Not Prepared", 552503)
 LSM:Register("sound", "Magtheridon: I am Unleashed", 554554)
 LSM:Register("sound", "Loatheb: I see you", 554236)
 LSM:Register("sound", "Ikiss: Trinkets", 561403)
+LSM:Register("sound", "Kobold: Not junk", 5726692)
 LSM:Register("sound", "NPCScan", 567275)--Sound file is actually bogus, this just forces the option NPCScan into menu. We hack it later.
 LSM:Register("sound", "PvP Alliance", 568320) -- PVPWarningAllianceLong
 LSM:Register("sound", "PvP Horde", 569112) -- PVPWarningHordeLong
