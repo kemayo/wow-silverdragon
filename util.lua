d2e9343ab4ab91443bff7cbb7bb3b374600436a5
David Lynch <kemayo@gmail.com>
2026-09-05 11:59:15 -0500

    GetClosestLocationForMob: respect per-coordinate conditions

--- util.lua
+++ util.lua
@@ -219,22 +219,25 @@ function addon:GetClosestLocationForMob(id)
 	local closest = {distance = 999999999}
 	for zone2, coords in pairs(ns.mobdb[id].locations) do
 		for i, coord in ipairs(coords) do
-			local x2, y2 = self:GetXY(coord)
-			local distance = HBD:GetZoneDistance(zone, x, y, zone2, x2, y2)
-			if not distance then
-				if not closest.zone then
-					-- make sure we get one
+			-- skip a spot the mob only stands in under a condition that isn't met
+			if self:CoordGateMet(ns.mobdb[id], zone2, coord) then
+				local x2, y2 = self:GetXY(coord)
+				local distance = HBD:GetZoneDistance(zone, x, y, zone2, x2, y2)
+				if not distance then
+					if not closest.zone then
+						-- make sure we get one
+						closest.zone = zone2
+						closest.x = x2
+						closest.y = y2
+					end
+					distance = 999999999
+				end
+				if distance < closest.distance then
+					closest.distance = distance
 					closest.zone = zone2
 					closest.x = x2
 					closest.y = y2
 				end
-				distance = 999999999
-			end
-			if distance < closest.distance then
-				closest.distance = distance
-				closest.zone = zone2
-				closest.x = x2
-				closest.y = y2
 			end
 		end
 	end
